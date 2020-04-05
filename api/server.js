c05f2976a09c50b6f9819eb02a4f9b8ceb326157
Dan Johnson <danimal.johnson@gmail.com>
2020-04-04 20:53:02 -0700

    chore: remove test axios call. Code cleanup.

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const axios = require('axios');
 
 const server = express();
 const apiRouter = require('./api-router.js');
@@ -14,22 +13,6 @@ server.use('/api', apiRouter);
 
 server.get('/', (req, res) => {
   res.json({ message: 'nCOV19 Vaccine Dashboard backend is live' });
-});
-
-// DEMO of polling an external API
-// Logs output to console.
-server.get('/axios', (req, res) => {
-  axios
-    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    .then((response) => {
-      console.log(response.data.url);
-      console.log(response.data.explanation);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
-  res.status(200).json({ message: 'ET phone home.' });
 });
 
 module.exports = server;
