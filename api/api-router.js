27f09108f54a98e94219cedb3a4401641223c02f
Dan Johnson <danimal.johnson@gmail.com>
2020-04-04 20:07:23 -0700

    feat: add previous and next objects for easy page navigation

--- api/api-router.js
+++ api/api-router.js
@@ -38,11 +38,13 @@ router.get('/totals', async (req, res) => {
 });
 
 // GET /api/trials
-//  Required query
-//
-//
+//   Required queries: type=[vaccines|treatments|alternatives]
+//   Optional queries: countries=[country_name]
+//   Optional pagination: page=x & limit=y
 router.get('/trials', (req, res) => {
-  const { type, countries, page, limit } = req.query;
+  const { type, countries } = req.query;
+  const page = parseInt(req.query.page, 10);
+  const limit = parseInt(req.query.limit, 10);
 
   if (type !== 'vaccines' && type !== 'treatments' && type !== 'alternatives') {
     res.status(400).json({ message: 'No valid trial type specified.' });
@@ -50,14 +52,32 @@ router.get('/trials', (req, res) => {
 
   db.findBy(type, countries)
     .then((info) => {
-      let results;
-      if (page !== undefined && limit !== undefined) {
+      const results = {};
+
+      if (limit > 0 && page > 0) {
+        // Apply pagination. Find start and end indices.
         const startIndex = (page - 1) * limit;
         const endIndex = page * limit;
-        console.log(page, limit, startIndex, endIndex);
-        results = info.slice(startIndex, endIndex);
+
+        // Create a "previous" object, if available
+        if (startIndex > 0) {
+          results.previous = {
+            page: page - 1,
+            limit,
+          };
+        }
+        // Create a "next" object, if available
+        if (endIndex < info.length) {
+          results.next = {
+            page: page + 1,
+            limit,
+          };
+        }
+        // Extract the requested section of the results
+        results.results = info.slice(startIndex, endIndex);
       } else {
-        results = info;
+        // No pagination. Send all the results.
+        results.results = info;
       }
       res.status(200).json(results);
     })
